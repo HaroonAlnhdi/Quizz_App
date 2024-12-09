09b6e202160fe0d88f0491d7beb8da8e41b900ea
Fadelm300 <fadel.m200@gmail.com>
2024-12-09 15:33:52 +0300

    searchQuery

--- lib/screens/admin/QuizsListPage.dart
+++ lib/screens/admin/QuizsListPage.dart
@@ -3,8 +3,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:quiz_app/screens/admin/AdminAppBar.dart';
 
-class QuizsListPage extends StatelessWidget {
+class QuizsListPage extends StatefulWidget {
   const QuizsListPage({super.key});
+
+  @override
+  State<QuizsListPage> createState() => _QuizsListPageState();
+}
+
+class _QuizsListPageState extends State<QuizsListPage> {
+  String searchQuery = ""; // Variable to store search input
 
   @override
   Widget build(BuildContext context) {
@@ -13,146 +20,188 @@ class QuizsListPage extends StatelessWidget {
 
     return Scaffold(
       appBar: const AdminAppBar(title: "Quizs List"),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('Exams')
-            .where('createdBy', isEqualTo: userEmail)
-            .snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return const Center(
-              child: Text(
-                "No quizzes available.",
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-            );
-          }
-
-          return ListView.separated(
-            padding: const EdgeInsets.all(10),
-            itemCount: snapshot.data!.docs.length,
-            separatorBuilder: (context, index) => const Divider(),
-            itemBuilder: (context, index) {
-              var doc = snapshot.data!.docs[index];
-
-              return Card(
-                color: Color.fromARGB(255, 246, 227, 253),
-                elevation: 4,
-                shape: RoundedRectangleBorder(
+      body: Column(
+        children: [
+          // Search Bar
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  searchQuery = value.toLowerCase();
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Search Quizzes',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ListTile(
-                        leading: const Icon(Icons.quiz,
-                            size: 40, color: Colors.blue),
-                        title: Text(
-                          doc['title'],
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        subtitle: Text(
-                          doc['description'],
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        trailing: PopupMenuButton(
-                          onSelected: (value) {
-                            if (value == 'edit') {
-                              // Add your edit functionality here
-                            } else if (value == 'delete') {
-                              FirebaseFirestore.instance
-                                  .collection('Exams')
-                                  .doc(doc.id)
-                                  .delete();
-                            }
-                          },
-                          itemBuilder: (context) => [
-                            const PopupMenuItem(
-                              value: 'edit',
-                              child: ListTile(
-                                leading: Icon(Icons.edit),
-                                title: Text('Edit'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Exams')
+                  .where('createdBy', isEqualTo: userEmail)
+                  .snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                  return const Center(
+                    child: Text(
+                      "No quizzes available.",
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                  );
+                }
+
+                // Filter quizzes based on the search query
+                final filteredDocs = snapshot.data!.docs.where((doc) {
+                  final title = (doc['title'] as String).toLowerCase();
+                  return title.contains(searchQuery);
+                }).toList();
+
+                if (filteredDocs.isEmpty) {
+                  return const Center(
+                    child: Text(
+                      "No quizzes match your search.",
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                  );
+                }
+
+                return ListView.separated(
+                  padding: const EdgeInsets.all(10),
+                  itemCount: filteredDocs.length,
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemBuilder: (context, index) {
+                    var doc = filteredDocs[index];
+
+                    return Card(
+                      color: const Color.fromARGB(255, 246, 227, 253),
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 5),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ListTile(
+                              leading: const Icon(Icons.quiz,
+                                  size: 40, color: Colors.blue),
+                              title: Text(
+                                doc['title'],
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              subtitle: Text(
+                                doc['description'],
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              trailing: PopupMenuButton(
+                                onSelected: (value) {
+                                  if (value == 'edit') {
+                                    // Add your edit functionality here
+                                  } else if (value == 'delete') {
+                                    FirebaseFirestore.instance
+                                        .collection('Exams')
+                                        .doc(doc.id)
+                                        .delete();
+                                  }
+                                },
+                                itemBuilder: (context) => [
+                                  const PopupMenuItem(
+                                    value: 'edit',
+                                    child: ListTile(
+                                      leading: Icon(Icons.edit),
+                                      title: Text('Edit'),
+                                    ),
+                                  ),
+                                  const PopupMenuItem(
+                                    value: 'delete',
+                                    child: ListTile(
+                                      leading: Icon(Icons.delete),
+                                      title: Text('Delete'),
+                                    ),
+                                  ),
+                                ],
+                                child: const Icon(Icons.more_vert),
                               ),
                             ),
-                            const PopupMenuItem(
-                              value: 'delete',
-                              child: ListTile(
-                                leading: Icon(Icons.delete),
-                                title: Text('Delete'),
-                              ),
+                            const SizedBox(height: 8),
+                            Row(
+                              children: [
+                                const Icon(Icons.person, color: Colors.grey),
+                                const SizedBox(width: 5),
+                                Expanded(
+                                  child: Text(
+                                      'Created By: ${doc['createdBy']}'),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 4),
+                            Row(
+                              children: [
+                                const Icon(Icons.timer, color: Colors.grey),
+                                const SizedBox(width: 5),
+                                Expanded(
+                                  child: Text(
+                                      'Start: ${doc['startTime']} - End: ${doc['endTime']}'),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 4),
+                            Row(
+                              children: [
+                                const Icon(Icons.history_toggle_off,
+                                    color: Colors.grey),
+                                const SizedBox(width: 5),
+                                Expanded(
+                                  child: Text(
+                                      'Submission Limit: ${doc['submissionLimit']}'),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 4),
+                            Row(
+                              children: [
+                                const Icon(Icons.calendar_today,
+                                    color: Colors.grey),
+                                const SizedBox(width: 5),
+                                Expanded(
+                                  child: Text(
+                                      'Created At: ${doc['createdAt'].toDate()}'),
+                                ),
+                              ],
                             ),
                           ],
-                          child: const Icon(Icons.more_vert),
                         ),
                       ),
-                      const SizedBox(height: 8),
-                      Row(
-                        children: [
-                          const Icon(Icons.person, color: Colors.grey),
-                          const SizedBox(width: 5),
-                          Expanded(
-                            child: Text('Created By: ${doc['createdBy']}'),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 4),
-                      Row(
-                        children: [
-                          const Icon(Icons.timer, color: Colors.grey),
-                          const SizedBox(width: 5),
-                          Expanded(
-                            child: Text(
-                                'Start: ${doc['startTime']} - End: ${doc['endTime']}'),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 4),
-                      Row(
-                        children: [
-                          const Icon(Icons.history_toggle_off,
-                              color: Colors.grey),
-                          const SizedBox(width: 5),
-                          Expanded(
-                            child: Text(
-                                'Submission Limit: ${doc['submissionLimit']}'),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 4),
-                      Row(
-                        children: [
-                          const Icon(Icons.calendar_today, color: Colors.grey),
-                          const SizedBox(width: 5),
-                          Expanded(
-                            child: Text(
-                                'Created At: ${doc['createdAt'].toDate()}'),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        },
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
       bottomSheet: Container(
         width: double.infinity,
-        color: Color(0xFF7826b5),
+        color: const Color(0xFF7826b5),
         height: 50,
         child: TextButton.icon(
           onPressed: () {
